34ec84342eefadf396ae8c13605b5d0772e79f45
Dalius Grazinskis <grazinskisd@gmail.com>
2018-10-24 17:59:38 +0200

    Add simple intro scene

---
+++ Assets/Scripts/IntroController.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+namespace JumpingJack
+{
+    public class IntroController: MonoBehaviour
+    {
+        public string GameSceneName;
+        public float WaitTime;
+
+        public void Start()
+        {
+            StartCoroutine(LoadGameScene());
+        }
+
+        private IEnumerator LoadGameScene()
+        {
+            yield return new WaitForSeconds(WaitTime);
+            SceneManager.LoadScene(GameSceneName);
+        }
+    }
+}
