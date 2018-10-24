db450fda2925070e1a4461b9d30d3ffb2cc485a7
Dalius Grazinskis <grazinskisd@gmail.com>
2018-10-24 21:14:41 +0200

    Cleanup next level scene

--- Assets/Scripts/Controllers/NextLevelController.cs
+++ Assets/Scripts/Controllers/NextLevelController.cs
@@ -1,16 +1,35 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using UnityEngine.UI;
 
 namespace JumpingJack
 {
     public class NextLevelController: MonoBehaviour
     {
+        private const string NEXT_LEVEL_FORMAT = "NEXT LEVEL - {0} HAZARD";
+
+        public Text NextLevelText;
+        public Text PhymeText;
+
         public float WaitTime;
 
         public void Start()
         {
+            UpdateHazardsText();
             StartCoroutine(LoadGameScene());
+        }
+
+        private void UpdateHazardsText()
+        {
+            string format = NEXT_LEVEL_FORMAT;
+            int hazards = PlayerPrefsService.GetInt(Prefs.Hazards);
+            if (hazards != 1)
+            {
+                format += "S";
+            }
+            NextLevelText.text = string.Format(format, hazards);
         }
 
         private IEnumerator LoadGameScene()
