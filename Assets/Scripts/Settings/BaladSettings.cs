5eb0635663cd853948611e75568205a72cdfbbd2
Dalius Grazinskis <grazinskisd@gmail.com>
2018-10-25 15:17:58 +0200

    Copy the balad

---
+++ Assets/Scripts/Settings/BaladSettings.cs
@@ -0,0 +1,11 @@
+﻿using UnityEngine;
+
+namespace JumpingJack
+{
+    [CreateAssetMenu(menuName = "JumpingJack/Balad")]
+    public class BaladSettings: ScriptableObject
+    {
+        [TextArea]
+        public string[] Rhyme;
+    }
+}
