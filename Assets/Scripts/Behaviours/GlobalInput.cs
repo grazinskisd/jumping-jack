6aae1d584aa4aa55558de3935aea275e38843e68
Dalius Grazinskis <grazinskisd@gmail.com>
2018-10-25 16:26:40 +0200

    Add some basic controlls
    - Esc quits the game
    - Sapce reload the game

---
+++ Assets/Scripts/Behaviours/GlobalInput.cs
@@ -0,0 +1,36 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+
+namespace JumpingJack
+{
+    public class GlobalInput: MonoBehaviour
+    {
+        private static bool _isCreated = false;
+
+        public void Awake()
+        {
+            if (!_isCreated)
+            {
+                DontDestroyOnLoad(gameObject);
+                _isCreated = true;
+            }
+            else
+            {
+                Destroy(gameObject);
+            }
+        }
+
+        private void Update()
+        {
+            if (Input.GetKey(KeyCode.Escape))
+            {
+                Application.Quit();
+            }
+
+            if (Input.GetKey(KeyCode.Space))
+            {
+                SceneManager.LoadScene(Scenes.IntroScene.ToString());
+            }
+        }
+    }
+}
