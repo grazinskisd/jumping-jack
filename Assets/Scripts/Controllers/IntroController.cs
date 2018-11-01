1b592dc129be89f39f9c1933199d722aae2d0316
Dalius Grazinskis <grazinskisd@gmail.com>
2018-11-01 13:10:09 +0100

    Better lvl cheat

--- Assets/Scripts/Controllers/IntroController.cs
+++ Assets/Scripts/Controllers/IntroController.cs
@@ -2,19 +2,24 @@
 using System.Collections;
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using UnityEngine.UI;
 
 namespace JumpingJack
 {
     public class IntroController: MonoBehaviour
     {
+        private const string LVL_CHEAT_FORMAT = "Lvl: {0}";
+
         public float WaitTime;
         public AppearingUILetters AppearingText;
         public AudioSource AudioSource;
         public AudioClip LetterRaise;
         public PeopleLetters PeopleLetters;
+        public Text CheatText;
 
         private int _level = 0;
         private PlayerPrefsService _prefService;
+        private bool _isLevelCheatActivated;
 
         private void Awake()
         {
@@ -23,6 +28,7 @@ namespace JumpingJack
 
         private void Start()
         {
+            CheatText.gameObject.SetActive(false);
             AppearingText.OnFinished += LoadGame;
             _prefService.ResetToStart();
             PeopleLetters.OnLettersRaising += OnLettersRaise;
@@ -47,26 +53,47 @@ namespace JumpingJack
         {
             if (Input.GetKeyDown(KeyCode.A))
             {
-                _level = Mathf.Min(_level + 1, 20);
-#if UNITY_EDITOR
-                Debug.Log("Level: " + _level);
-#endif
+                _level = Mathf.Min(_level + 1, _prefService.MaxLevel);
+                if (!_isLevelCheatActivated)
+                {
+                    ActivateLvlCheatDisplay();
+                }
+                CheatText.text = string.Format(LVL_CHEAT_FORMAT, _level);
             }
 
             if (Input.GetKeyDown(KeyCode.G))
             {
                 _prefService.SetInt(Prefs.GodMode, 1);
             }
+
+            if (Input.GetKeyDown(KeyCode.Return) && _isLevelCheatActivated)
+            {
+                LoadGameImmediate();
+            }
+        }
+
+        private void ActivateLvlCheatDisplay()
+        {
+            CheatText.gameObject.SetActive(true);
+            _isLevelCheatActivated = true;
         }
 
         private void LoadGame()
         {
-            StartCoroutine(LoadGameScene());
+            if (!_isLevelCheatActivated)
+            {
+                StartCoroutine(LoadGameScene());
+            }
         }
 
         private IEnumerator LoadGameScene()
         {
             yield return new WaitForSeconds(WaitTime);
+            LoadGameImmediate();
+        }
+
+        private void LoadGameImmediate()
+        {
             _prefService.SetLevelTo(_level);
             SceneManager.LoadScene(Scenes.GameScene.ToString());
         }
